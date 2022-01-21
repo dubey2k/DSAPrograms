879c8d0c37baf4092ee5901147c0aafd1459cda2
dubek2k <amitkd2018@gmail.com>
2022-01-21 22:42:07 +0530

    init

---
+++ FirstMissingPositiveNumber.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+
+public class FirstMissingPositiveNumber{
+    public static void main(String[] args){
+        int[] arr = {3,4,-1,1};
+		System.out.println(firstMissingPositive(arr));
+    }
+    public static int firstMissingPositive(int[] nums) {
+        boolean isFirst = false;
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 1)
+                isFirst = true;
+            else if(nums[i] > n || nums[i] <= 0){
+                nums[i] = 1;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            nums[Math.abs(nums[i]) - 1] = -Math.abs(nums[Math.abs(nums[i]) - 1]);
+        }
+        if(!isFirst)
+            return 1;
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0)
+                return i + 1;
+        }
+        return n + 1;
+    }
+}
